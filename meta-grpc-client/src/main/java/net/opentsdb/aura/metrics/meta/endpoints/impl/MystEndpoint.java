40fb70c03be2702bf745b2074ee47e01050c22e4
ntippabhatla <niharika.tippabhatla@verizonmedia.com>
2021-10-12 10:45:26 -0700

    GetListOfMystEndPoints

--- meta-grpc-client/src/main/java/net/opentsdb/aura/metrics/meta/endpoints/impl/MystEndpoint.java
+++ meta-grpc-client/src/main/java/net/opentsdb/aura/metrics/meta/endpoints/impl/MystEndpoint.java
@@ -1,7 +1,26 @@
+/*
+ * This file is part of OpenTSDB.
+ * Copyright (C) 2021  Yahoo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.opentsdb.aura.metrics.meta.endpoints.impl;
 
 import net.opentsdb.aura.metrics.meta.endpoints.ShardEndPoint;
-//TODO: Make a builder
+
+import java.util.Objects;
+
+
 public class MystEndpoint implements ShardEndPoint {
 
     private final String host;
@@ -27,6 +46,30 @@ public class MystEndpoint implements ShardEndPoint {
     public Protocol getProtocol() {
         return Protocol.http2_0;
     }
+
+    @Override
+    public boolean equals(Object that){
+        if (Objects.isNull(that)){
+            return false;
+        }
+        if (this == that){
+            return  true;
+        }else if (!(that instanceof ShardEndPoint)) {
+            return false;
+
+        }else{
+            if (Objects.equals(this.getHost(), ((ShardEndPoint) that).getHost()) &&
+                    Objects.equals(this.getPort(), ((ShardEndPoint) that).getPort())
+                    && Objects.equals(this.getProtocol(), ((ShardEndPoint) that).getProtocol()) &&
+                    ((ShardEndPoint) that).mtls() == false && this.mtls() == false ){
+
+                        return true;
+            }else{
+                        return false;
+            }
+        }
+}
+
 
     @Override
     public boolean mtls() {
@@ -55,5 +98,9 @@ public class MystEndpoint implements ShardEndPoint {
             return new Builder();
         }
 
+    }
+    @Override
+    public String toString() {
+        return "Endpoints {host=" + host + ", port=" + port +"}";
     }
 }
--- meta-grpc-client/src/main/java/net/opentsdb/aura/metrics/meta/endpoints/impl/MystStatefulSetRegistry.java
+++ meta-grpc-client/src/main/java/net/opentsdb/aura/metrics/meta/endpoints/impl/MystStatefulSetRegistry.java
@@ -1,34 +1,52 @@
+/*
+ * This file is part of OpenTSDB.
+ * Copyright (C) 2021  Yahoo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.opentsdb.aura.metrics.meta.endpoints.impl;
 
 import net.opentsdb.aura.metrics.meta.endpoints.ShardEndPoint;
+import net.opentsdb.aura.metrics.meta.endpoints.ShardedServiceRegistry;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-//TODO:
-// make a cache
-// Refetch for a namespace only if things have changed
-// Write testcases using Mocks (jmockit or mockito)
-public class MystStatefulSetRegistry extends KubernetesStatefulRegistry {
+import java.util.*;
 
-    private static final String pod_name_pattern = "%s-myst-%s-%s.svc.%s";
+
+public class MystStatefulSetRegistry implements ShardedServiceRegistry {
+
+    private static final String pod_name_pattern = "%s-myst-%s-%s.%s.svc.%s";
+
+    private DeploymentConfig config;
+
+    public MystStatefulSetRegistry(DeploymentConfig config) {
+        this.config=config;
+    }
 
     public MystStatefulSetRegistry() {
-        super("myst");
     }
 
     @Override
     public Map<String, List<ShardEndPoint>> getEndpoints(String namespace) {
         Map<String, List<ShardEndPoint>> endpointsMap = new HashMap<>();
 
-        final int numShards = getNumShards(namespace);
-        final int numReplicas = getNumReplicas(namespace);
+        final int numShards = config.toMap().get(namespace).toMap().get("myst").getNumShards();
+        final int numReplicas = config.toMap().get(namespace).toMap().get("myst").getNumReplicas();
         for(int i = 0; i < numReplicas; i++) {
-            String replica = getReplica(i);
+            String replica = config.toMap().get(namespace).toMap().get("myst").getReplica(i);
             List<ShardEndPoint> shardEndPoints = new ArrayList<>();
             endpointsMap.put(replica, shardEndPoints);
-            for (int j = 0; j < numShards; i++) {
+            for (int j = 0; j < numShards; j++) {
                 final MystEndpoint endpoint = MystEndpoint.Builder.newBuilder()
                         .withHost(
                                 String.format(
@@ -36,10 +54,12 @@ public class MystStatefulSetRegistry extends KubernetesStatefulRegistry {
                                         namespace,
                                         replica,
                                         j,
-                                        getDomain(namespace)))
-                        .withPort(getPort(namespace))
+                                        namespace,
+                                        config.getClusterDomain()))
+                        .withPort(config.toMap().get(namespace).toMap().get("myst").getPort())
                         .build();
                 shardEndPoints.add(endpoint);
+
             }
         }
         return endpointsMap;
